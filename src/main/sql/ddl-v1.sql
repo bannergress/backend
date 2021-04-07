5d1d62a0952a43410ca00945a520ae5aa99abe2d
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-04-07 19:54:02 +0200

    Create schema definition for PostgreSQL.

---
+++ src/main/sql/ddl-v1.sql
@@ -0,0 +1,232 @@
+BEGIN;
+
+
+CREATE EXTENSION pg_trgm;
+
+
+CREATE SEQUENCE "hibernate_sequence";
+
+
+CREATE TYPE faction AS ENUM ('enlightened', 'resistance');
+CREATE CAST (varchar AS faction) WITH INOUT AS IMPLICIT;
+
+CREATE TYPE mission_type AS ENUM ('sequential', 'anyOrder', 'hidden');
+CREATE CAST (varchar AS mission_type) WITH INOUT AS IMPLICIT;
+
+CREATE TYPE objective AS ENUM ('hack', 'captureOrUpgrade', 'createLink', 'createField', 'installMod', 'takePhoto', 'viewWaypoint', 'enterPassphrase');
+CREATE CAST (varchar AS objective) WITH INOUT AS IMPLICIT;
+
+CREATE TYPE place_type AS ENUM ('country', 'administrative_area_level_1', 'administrative_area_level_2', 'administrative_area_level_3', 'administrative_area_level_4', 'administrative_area_level_5', 'locality');
+CREATE CAST (varchar AS place_type) WITH INOUT AS IMPLICIT;
+
+CREATE TYPE poi_type AS ENUM ('portal', 'fieldTripWaypoint', 'unavailable');
+CREATE CAST (varchar AS poi_type) WITH INOUT AS IMPLICIT;
+
+
+CREATE TABLE "banner_picture" (
+  "hash" text NOT NULL,
+  "picture" "oid" NOT NULL,
+  PRIMARY KEY ("hash")
+);
+
+
+CREATE TABLE "banner" (
+  "id" bigint NOT NULL,
+  "complete" boolean NOT NULL,
+  "created" timestamp with time zone NOT NULL,
+  "description" text,
+  "length_meters" double precision,
+  "number_of_missions" integer NOT NULL,
+  "online" boolean NOT NULL,
+  "start_latitude" double precision,
+  "start_longitude" double precision,
+  "title" text NOT NULL,
+  "picture" text,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("picture") REFERENCES "banner_picture"("hash")
+);
+CREATE INDEX ON "banner" ("created" DESC);
+CREATE INDEX ON "banner" ("start_latitude", "start_longitude");
+CREATE INDEX ON "banner" USING gin ((lower("title")) gin_trgm_ops);
+
+
+CREATE TABLE "place" (
+  "id" text NOT NULL,
+  "type" place_type NOT NULL,
+  PRIMARY KEY ("id")
+);
+CREATE INDEX ON "place" ("type");
+
+
+CREATE TABLE "place_information" (
+  "id" bigint NOT NULL,
+  "formatted_address" text NOT NULL,
+  "language_code" text NOT NULL,
+  "long_name" text NOT NULL,
+  "short_name" text NOT NULL,
+  "place" text NOT NULL,
+  PRIMARY KEY ("id"),
+  UNIQUE ("place", "language_code"),
+  FOREIGN KEY ("place") REFERENCES "place"("id")
+);
+CREATE INDEX ON "place_information" USING gin ((lower("long_name")) gin_trgm_ops);
+
+
+CREATE TABLE "banner_start_place" (
+  "banner" bigint NOT NULL,
+  "place" text NOT NULL,
+  PRIMARY KEY ("banner", "place"),
+  FOREIGN KEY ("banner") REFERENCES "banner"("id"),
+  FOREIGN KEY ("place") REFERENCES "place"("id")
+);
+CREATE INDEX ON "banner_start_place" ("place", "banner");
+
+
+CREATE TABLE "poi" (
+  "id" text NOT NULL,
+  "latitude" double precision,
+  "longitude" double precision,
+  "picture_url" text,
+  "title" text,
+  "type" poi_type NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+
+CREATE TABLE "named_agent" (
+  "name" text NOT NULL,
+  "faction" faction NOT NULL,
+  PRIMARY KEY ("name")
+);
+CREATE INDEX ON "named_agent" ("name", "faction");
+
+
+CREATE TABLE "mission" (
+  "id" text NOT NULL,
+  "average_duration_milliseconds" bigint NOT NULL,
+  "description" text,
+  "latest_update_details" timestamp with time zone,
+  "latest_update_summary" timestamp with time zone,
+  "number_completed" integer,
+  "online" boolean NOT NULL,
+  "picture_url" text NOT NULL,
+  "rating" double precision NOT NULL,
+  "title" text NOT NULL,
+  "type" mission_type,
+  "author" text,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("author") REFERENCES "named_agent"("name")
+);
+CREATE INDEX ON "mission" USING gin ((lower("title")) gin_trgm_ops);
+
+
+CREATE TABLE "mission_step" (
+  "id" bigint NOT NULL,
+  "objective" objective,
+  "mission" text NOT NULL,
+  "poi" text,
+  "position" integer,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("mission") REFERENCES "mission"("id"),
+  FOREIGN KEY ("poi") REFERENCES "poi"("id")
+);
+CREATE INDEX ON "mission_step" ("mission", "poi", "id", "objective", "position");
+CREATE INDEX ON "mission_step" ("poi");
+
+
+CREATE TABLE "banner_mission" (
+  "banner" bigint NOT NULL,
+  "mission" text NOT NULL,
+  "position" integer NOT NULL,
+  PRIMARY KEY ("banner", "position"),
+  FOREIGN KEY ("banner") REFERENCES "banner"("id"),
+  FOREIGN KEY ("mission") REFERENCES "mission"("id")
+);
+CREATE INDEX ON "banner_mission" ("banner", "mission", "position");
+CREATE INDEX ON "banner_mission" ("mission");
+
+
+CREATE TABLE "revision" (
+  "id" integer NOT NULL,
+  "created" timestamp with time zone NOT NULL,
+  "userid" text,
+  PRIMARY KEY ("id")
+);
+
+
+CREATE TABLE "banner_audit" (
+  "id" bigint NOT NULL,
+  "rev" integer NOT NULL,
+  "revtype" smallint,
+  "description" text,
+  "number_of_missions" integer,
+  "title" text,
+  PRIMARY KEY ("id", "rev"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+CREATE TABLE "banner_mission_audit" (
+  "rev" integer NOT NULL,
+  "banner" bigint NOT NULL,
+  "mission" text NOT NULL,
+  "position" integer NOT NULL,
+  "revtype" smallint,
+  PRIMARY KEY ("rev", "banner", "mission", "position"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+CREATE TABLE "mission_audit" (
+  "id" text NOT NULL,
+  "rev" integer NOT NULL,
+  "revtype" smallint,
+  "description" text,
+  "online" boolean,
+  "picture_url" text,
+  "title" text,
+  "type" mission_type,
+  "author" text,
+  PRIMARY KEY ("id", "rev"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+CREATE TABLE "mission_step_audit" (
+  "id" bigint NOT NULL,
+  "rev" integer NOT NULL,
+  "revtype" smallint,
+  "objective" objective,
+  "position" integer,
+  "mission" text,
+  "poi" text,
+  PRIMARY KEY ("id", "rev"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+CREATE TABLE "named_agent_audit" (
+  "name" text NOT NULL,
+  "rev" integer NOT NULL,
+  "revtype" smallint,
+  "faction" faction,
+  PRIMARY KEY ("name", "rev"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+CREATE TABLE "poi_audit" (
+  "id" text NOT NULL,
+  "rev" integer NOT NULL,
+  "revtype" smallint,
+  "latitude" double precision,
+  "longitude" double precision,
+  "picture_url" text,
+  "title" text,
+  "type" poi_type,
+  PRIMARY KEY ("id", "rev"),
+  FOREIGN KEY ("rev") REFERENCES "revision"("id")
+);
+
+
+COMMIT;
