d29573b1fe1632e8f8adea496e2aea61b10af62f
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-04-19 15:16:07 +0200

    Re-enable web autoconfiguration.

--- src/main/java/com/bannergress/backend/config/Webconfiguration.java
+++ src/main/java/com/bannergress/backend/config/Webconfiguration.java
@@ -2,16 +2,14 @@ package com.bannergress.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class Webconfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        //Allows CORS from all enpoints to any origin
+        // Allows CORS from all endpoints to any origin
         registry.addMapping("/**");
     }
 }
