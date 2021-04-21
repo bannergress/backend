97dc8ea9ffabdfd6c2a54186e04a5c60941f1fb7
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-04-21 07:48:12 +0200

    Use 409 conflict for already used missions.

--- src/main/java/com/bannergress/backend/exceptions/MissionAlreadyUsedException.java
+++ src/main/java/com/bannergress/backend/exceptions/MissionAlreadyUsedException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * Exception for when a mission is already used by another banner.
  */
-@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class MissionAlreadyUsedException extends Exception {
     private static final long serialVersionUID = 1L;
 }
