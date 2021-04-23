d6236c31e677039cff3e5d510b9571951d302de0
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-04-23 14:02:28 +0200

    Change controller to only handle /error and return the dispatcher error.

--- src/main/java/com/bannergress/backend/controllers/DefaultController.java
+++ src/main/java/com/bannergress/backend/controllers/DefaultController.java
@@ -1,17 +1,28 @@
 package com.bannergress.backend.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 /**
- * Returns 404 NOT FOUND with no content for any request to unknown resources.
+ * Handles error requests.
  */
 @RestController
-public class DefaultController  {
+public class DefaultController implements ErrorController {
+    @RequestMapping(value = "/error")
+    public ResponseEntity<Void> error(HttpServletRequest request) {
+        Integer errorCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return errorCode == null ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            : ResponseEntity.status(errorCode).build();
+    }
 
-    @RequestMapping(value = "/**")
-    public ResponseEntity<String> notFound() {
-        return ResponseEntity.notFound().build();
+    @Override
+    public String getErrorPath() {
+        return null;
     }
 }
