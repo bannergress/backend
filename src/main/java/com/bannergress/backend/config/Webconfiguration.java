8d57657cca0361828e0f357ec5e3a8dca0382022
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-06-16 12:09:01 +0200

    Switch response production to JSON by default, and ignore accept header.

--- src/main/java/com/bannergress/backend/config/Webconfiguration.java
+++ src/main/java/com/bannergress/backend/config/Webconfiguration.java
@@ -1,6 +1,8 @@
 package com.bannergress.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,11 @@ public class Webconfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allows CORS from all endpoints to any origin
         registry.addMapping("/**").allowedMethods("*");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON, MediaType.ALL);
+        configurer.ignoreAcceptHeader(true);
     }
 }
