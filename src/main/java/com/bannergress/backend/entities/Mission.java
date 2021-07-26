5bb9f3c97174b11ca3be04f25cdd62c2940153f1
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-07-26 10:52:51 +0200

    Set new missions online by default.

--- src/main/java/com/bannergress/backend/entities/Mission.java
+++ src/main/java/com/bannergress/backend/entities/Mission.java
@@ -96,7 +96,7 @@ public class Mission {
      * Mission status.
      */
     @Column(name = "online", nullable = false)
-    private boolean online;
+    private boolean online = true;
 
     /**
      * Timestamp when the mission summary was last updated.
