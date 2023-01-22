f160e9ba1aa70642cca15322af8dd8e1dbc10b73
Eckhart Wörner <ewoerner@users.noreply.github.com>
2023-01-22 17:05:11 +0100

    Deserialize intel missions using @JsonFormat(shape = JsonFormat.Shape.ARRAY) Closes #292

--- src/main/java/com/bannergress/backend/dto/IntelMissionDetails.java
+++ src/main/java/com/bannergress/backend/dto/IntelMissionDetails.java
@@ -1,8 +1,13 @@
 package com.bannergress.backend.dto;
 
-import com.bannergress.backend.dto.serialization.IntelMissionDetailsDeserializer;
+import com.bannergress.backend.dto.serialization.IntelFactionDeserializer;
+import com.bannergress.backend.dto.serialization.IntelMissionStepDeserializer;
+import com.bannergress.backend.dto.serialization.IntelMissionTypeDeserializer;
 import com.bannergress.backend.enums.Faction;
 import com.bannergress.backend.enums.MissionType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,19 +15,24 @@ import javax.validation.constraints.NotNull;
 
 import java.util.List;
 
-@JsonDeserialize(using = IntelMissionDetailsDeserializer.class)
+@JsonFormat(shape = Shape.ARRAY)
+@JsonPropertyOrder({"id", "title", "description", "authorName", "authorFaction", "ratingE6",
+    "averageDurationMilliseconds", "numberCompleted", "type", "steps", "picture"})
 public class IntelMissionDetails extends IntelMissionSummary {
     public String description;
 
     public String authorName;
 
+    @JsonDeserialize(using = IntelFactionDeserializer.class)
     public Faction authorFaction;
 
     public int numberCompleted;
 
     @NotNull
+    @JsonDeserialize(using = IntelMissionTypeDeserializer.class)
     public MissionType type;
 
     @NotEmpty
+    @JsonDeserialize(contentUsing = IntelMissionStepDeserializer.class)
     public List<IntelMissionStep> steps;
 }
