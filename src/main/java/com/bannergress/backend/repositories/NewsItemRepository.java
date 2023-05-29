9fe0dbc9f95b5a341add11912e97d9b2eab92e44
Eckhart Wörner <ewoerner@users.noreply.github.com>
2023-05-29 09:43:35 +0200

    Remove unnecessary @Autowired and @Repository annotations.

--- src/main/java/com/bannergress/backend/repositories/NewsItemRepository.java
+++ src/main/java/com/bannergress/backend/repositories/NewsItemRepository.java
@@ -2,14 +2,12 @@ package com.bannergress.backend.repositories;
 
 import com.bannergress.backend.entities.NewsItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 /**
  * Repository for news items.
  */
-@Repository
 public interface NewsItemRepository extends JpaRepository<NewsItem, UUID> {
 
 }
