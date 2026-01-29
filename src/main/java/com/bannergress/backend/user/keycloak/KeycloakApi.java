a91bbac26ac5495fb98403af30020e40d0ce9f7d
Eckhart Wörner <ewoerner@users.noreply.github.com>
2026-01-29 23:48:49 +0100

    Refactor keycloak user implementation to HttpExchange autoconfiguration

--- src/main/java/com/bannergress/backend/user/keycloak/KeycloakApi.java
+++ src/main/java/com/bannergress/backend/user/keycloak/KeycloakApi.java
@@ -1,10 +1,12 @@
 package com.bannergress.backend.user.keycloak;
 
+import org.springframework.security.oauth2.client.annotation.ClientRegistrationId;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PutExchange;
 
+@ClientRegistrationId("keycloak")
 public interface KeycloakApi {
     @GetExchange("/users/{userId}")
     KeycloakUserRepresentation getUser(@PathVariable String userId);
