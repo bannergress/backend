e816fc1fc5f1d195342fad0af24358b57f50cdb4
Eckhart Wörner <ewoerner@users.noreply.github.com>
2026-02-22 20:21:32 +0100

    Use ExpressionJwtGrantedAuthoritiesConverter for extracting Keycloak roles

--- src/main/java/com/bannergress/backend/security/KeycloakSecurityConfig.java
+++ src/main/java/com/bannergress/backend/security/KeycloakSecurityConfig.java
@@ -2,27 +2,30 @@ package com.bannergress.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.CacheControlConfig;
+import org.springframework.security.oauth2.server.resource.authentication.ExpressionJwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /** Keycloak security configuration. */
 @Configuration
 @EnableMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
 class KeycloakSecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationConverter jwtAuthenticationConverter) {
         return http //
             .csrf(CsrfConfigurer::disable) //
-            .headers(this::customizeHeaders)
-            .oauth2ResourceServer(
-                oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))) //
+            .headers(this::customizeHeaders) //
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())) //
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) //
             .build();
     }
@@ -33,9 +36,13 @@ class KeycloakSecurityConfig {
             .addHeaderWriter(new StaticHeadersWriter(HttpHeaders.CACHE_CONTROL, "must-revalidate"));
     }
 
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new ExpressionJwtGrantedAuthoritiesConverter(
+            new SpelExpressionParser().parseRaw("[realm_access][roles]"));
+        converter.setAuthorityPrefix("ROLE_");
         JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
         return jwtConverter;
     }
 }
